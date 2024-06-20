d6ce3d9fd0df919a79e2fa926bf491932ce1ee2b
Lisanoti <mdnisanofficial@gmail.com>
2024-06-20 16:11:58 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ ⚪🔴⚫ ‖ sʜᴇɪᴋʜ ʀᴜᴘᴏᴋ ‖ ⚫🔴⚪ ]";
+const doNotDelete = "[ ⚪🔴⚫ ‖ LISA BOT ‖ ⚫🔴⚪ ]";
 /**
 * @author NTKhang
 * @author: do not delete it
